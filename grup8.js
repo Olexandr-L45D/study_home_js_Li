8af5114a34c77ac7b0145f90f476d3a0f02e261c
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-07-25 13:05:50 +0300

    2 commet

--- grup8.js
+++ grup8.js
@@ -1,3 +1,32 @@
-// my sechin About My
+// my sekcion About My
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+
+const accordion = new Accordion('.accordion-container-first', {
+    duration: 400,
+    showMultiple: true,
+    onOpen: function(currentElement) {
+      console.log(currentElement);
+    }});// створив новий екземпляр цієї біліотеки,класу
+
+
+// // User 
+// new Accordion('.container-second', {
+//     duration: 400,
+//     showMultiple: true,
+//     onOpen: function(currentElement) {
+//       console.log(currentElement);
+//     }
+//   });
+  
+//   // Define several accordions with the same options (pass an array with selectors)
+//   new Accordion(['.container-first', '.container-second'], {});
+  
+//   // or pass an array with HTMLElements
+//   const accordions = Array.from(document.querySelectorAll('.accordion-container'));
+//   new Accordion(accordions, {});
+  
+//   // Detach events
+//   const accordion = new Accordion('.container-first');
+//   accordion.detachEvents();
+  
